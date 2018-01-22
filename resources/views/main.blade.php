0cbcce72840298e0cdbc955fb0a2c1fc4d55dbc9
Farzad Rahman <farzadrahman59@gmail.com>
2018-01-22 15:12:27 +0600

    ready to push

--- resources/views/main.blade.php
+++ resources/views/main.blade.php
@@ -35,11 +35,13 @@
     <link href="{{asset('css/style.css')}}" rel="stylesheet">
     <!-- You can change the theme colors from here -->
     <link href="{{asset('css/colors/blue.css')}}" id="theme" rel="stylesheet">
+    <link rel="stylesheet" href="{{asset('date/date-picker.css')}}">
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
     <!--[if lt IE 9]>
     <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
     <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
+
     <![endif]-->
 
     <style>
@@ -105,6 +107,10 @@
             @if(Session::has('message'))
                 <p class="alert {{ Session::get('alert-class', 'alert-success') }}">{{ Session::get('message') }}</p>
             @endif
+
+            <div class="alert alert-warning" style="display: none ">
+                <strong>Success!</strong> Indicates a successful or positive action.
+            </div>
          @yield('content')
             <!-- ============================================================== -->
             <!-- End Right sidebar -->
@@ -154,6 +160,8 @@
 <script src="{{asset('assets/plugins/sticky-kit-master/dist/sticky-kit.min.js')}}"></script>
 <!--Custom JavaScript -->
 <script src="{{asset('js/custom.min.js')}}"></script>
+<script src="{{asset('date/jquery-ui.js')}}"></script>
+<script src="{{asset('date/script.js')}}"></script>
 
 <!-- ============================================================== -->
 <!-- This page plugins -->
