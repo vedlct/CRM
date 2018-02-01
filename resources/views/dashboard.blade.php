0012a1794825ad790167e87163c24e2b041a97e1
Farzad Rahman <farzadrahman59@gmail.com>
2018-02-01 17:44:01 +0600

    commited

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -5,12 +5,15 @@
 @section('content')
 
 
-    <div id="chart-div"></div>
+    {{--<div id="chart-div"></div>--}}
 
-    <?= $lava->render('PieChart', 'IMDB', 'chart-div') ?>
+    {{--<?//= $lava->render('PieChart', 'IMDB', 'chart-div') ?>--}}
 
 
-
+    <div class="card" style="padding:10px;">
+        <div class="card-body">
+            <h2 align="center"><b>THIS PART WILL BE INCLUDED IN NEXT UPDATE</b></h2>
+        </div></div>
 
 
 
