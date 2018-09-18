721be984f2030fc3b0128e64f732e6ac1a400638
sakib <md.sakibrahman@gmail.com>
2018-09-18 18:23:43 +0600

    sakib

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -59,8 +59,62 @@ class LoginController extends Controller
 
 
 
+//    public function login(\Illuminate\Http\Request $request) {
+//
+//        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+//        // the login attempts for this application. We'll key this by the username and
+//        // the IP address of the client making these requests into this application.
+//        if ($this->hasTooManyLoginAttempts($request)) {
+//            $this->fireLockoutEvent($request);
+//            return $this->sendLockoutResponse($request);
+//        }
+//
+//
+//        // This section is the only change
+//        if ($this->guard()->validate($this->credentials($request))) {
+//            $user = $this->guard()->getLastAttempted();
+//
+//
+//           // $type=strtoupper(Auth::user()->userType->typeName);
+//            //return $user;
+//            // Make sure the user is active
+//            if ($user->active && $this->attemptLogin($request)) {
+//                // Send the normal successful login response
+//                $type=strtoupper(Auth::user()->userType->typeName);
+//                Session::put('userType',$type);
+//                $ip = \Request::ip();
+//                if ($ip != "::1" ) {
+//                    return $this->sendLoginResponse($request);
+//                }elseif($type == "ADMIN" && $ip != "::1") {
+//                    return $this->sendLoginResponse($request);
+//                }
+//                else{
+//                    return "cvcvcvc";
+//                    $this->incrementLoginAttempts($request);
+//                    return redirect()
+//                        ->back()
+//                        ->withInput($request->only($this->username(), 'remember'))
+//                        ->withErrors(['active' => 'You must be active to login.']);
+//                }
+//            } else {
+//                // Increment the failed login attempts and redirect back to the
+//                // login form with an error message.
+//                $this->incrementLoginAttempts($request);
+//                return redirect()
+//                    ->back()
+//                    ->withInput($request->only($this->username(), 'remember'))
+//                    ->withErrors(['active' => 'You must be active to login.']);
+//            }
+//        }
+//
+//        // If the login attempt was unsuccessful we will increment the number of attempts
+//        // to login and redirect the user back to the login form. Of course, when this
+//        // user surpasses their maximum number of attempts they will get locked out.
+//        $this->incrementLoginAttempts($request);
+//
+//        return $this->sendFailedLoginResponse($request);
+//    }
     public function login(\Illuminate\Http\Request $request) {
-        $this->validateLogin($request);
 
         // If the class is using the ThrottlesLogins trait, we can automatically throttle
         // the login attempts for this application. We'll key this by the username and
@@ -70,34 +124,71 @@ class LoginController extends Controller
             return $this->sendLockoutResponse($request);
         }
 
+
+
+
+
         // This section is the only change
         if ($this->guard()->validate($this->credentials($request))) {
             $user = $this->guard()->getLastAttempted();
 
-            // Make sure the user is active
-            if ($user->active && $this->attemptLogin($request)) {
-                // Send the normal successful login response
-                $type=strtoupper(Auth::user()->userType->typeName);
-                Session::put('userType',$type);
+            $ip = \Request::ip();
+//            if ((substr($ip,0,8) == "192.168.") || ($ip == "127.0.0.1")) {
+//                // client is local
+//            } else {
+//                // client is not local
+//            }
+//
+
+            if (substr($ip,0,8) == "192.168." || $ip == "::1") {
+
+                if ($user->active && $this->attemptLogin($request)) {
+                    // Send the normal successful login response
+                    $type = strtoupper(Auth::user()->userType->typeName);
+                    Session::put('userType', $type);
+                    return $this->sendLoginResponse($request);
+                } else {
+                    // Increment the failed login attempts and redirect back to the
+                    // login form with an error message.
+                    $this->incrementLoginAttempts($request);
+                    return redirect()
+                        ->back()
+                        ->withInput($request->only($this->username(), 'remember'))
+                        ->withErrors(['active' => 'You must be active to login.']);
+                }
+
 
                 return $this->sendLoginResponse($request);
-            } else {
-                // Increment the failed login attempts and redirect back to the
-                // login form with an error message.
-                $this->incrementLoginAttempts($request);
-                return redirect()
-                    ->back()
-                    ->withInput($request->only($this->username(), 'remember'))
-                    ->withErrors(['active' => 'You must be active to login.']);
+            } elseif ($user->typeId == "1" && substr($ip,0,8) != "192.168.") {
+
+                if ($user->active && $this->attemptLogin($request)) {
+                    // Send the normal successful login response
+                    $type = strtoupper(Auth::user()->userType->typeName);
+                    Session::put('userType', $type);
+                    return $this->sendLoginResponse($request);
+                } else {
+                    // Increment the failed login attempts and redirect back to the
+                    // login form with an error message.
+                    $this->incrementLoginAttempts($request);
+                    return redirect()
+                        ->back()
+                        ->withInput($request->only($this->username(), 'remember'))
+                        ->withErrors(['active' => 'You must be active to login.']);
+                }
+
             }
         }
 
         // If the login attempt was unsuccessful we will increment the number of attempts
         // to login and redirect the user back to the login form. Of course, when this
         // user surpasses their maximum number of attempts they will get locked out.
-        $this->incrementLoginAttempts($request);
+            $this->incrementLoginAttempts($request);
 
-        return $this->sendFailedLoginResponse($request);
+            //$this->sendFailedLoginResponse($request);
+        return redirect()
+            ->back()
+            ->withInput($request->only($this->username(), 'remember'))
+            ->withErrors(['userId' => 'Permission Denied']);
     }
 
 
