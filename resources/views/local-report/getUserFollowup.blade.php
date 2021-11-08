c3714ff5ffbc4c55a619e79089773a4cb005f914
Sazzad171 <sajjad.cse.diu@gmail.com>
2021-11-08 19:43:27 +0600

    update

--- resources/views/local-report/getUserFollowup.blade.php
+++ resources/views/local-report/getUserFollowup.blade.php
@@ -1,16 +1,18 @@
-<table class="table table-bordered table-striped">
-    <thead>
-    <th>Lead Name</th>
-    <th>Company Name</th>
-    <th>Date</th>
-    </thead>
-    <tbody>
-    @foreach($followup as $follow)
-        <tr>
-            <td>{{$follow->leadName}}</td>
-            <td>{{$follow->companyName}}</td>
-            <td>{{$follow->date}}</td>
-        </tr>
-    @endforeach
-    </tbody>
-</table>
+<div class="table-responsive">
+    <table class="table table-bordered table-striped">
+        <thead>
+        <th>Lead Name</th>
+        <th>Company Name</th>
+        <th>Date</th>
+        </thead>
+        <tbody>
+        @foreach($followup as $follow)
+            <tr>
+                <td>{{$follow->leadName}}</td>
+                <td>{{$follow->companyName}}</td>
+                <td>{{$follow->date}}</td>
+            </tr>
+        @endforeach
+        </tbody>
+    </table>
+</div>
--- resources/views/local-report/getUserRevenueLog.blade.php
+++ resources/views/local-report/getUserRevenueLog.blade.php
@@ -1,23 +1,25 @@
-<table class="table table-bordered table-striped" id="assigndatatable">
-    <thead>
-    <th>Lead Name</th>
-    <th>Company Name</th>
-    <th>Category</th>
-    <th>Area</th>
-    <th>Payment</th>
-    <th>Date</th>
-    </thead>
-    <tbody>
-        @foreach($bills as $bill)
-            <tr>
-                <th>{{$bill->leadName}}</th>
-                <th>{{$bill->companyName}}</th>
-                <th>{{$bill->categoryName}}</th>
-                <th>{{$bill->areaName}}</th>
-                <th>{{$bill->paymentInserted}}</th>
-                <th>{{$bill->paymentDate}}</th>
+<div class="table-responsive">
+    <table class="table table-bordered table-striped" id="assigndatatable">
+        <thead>
+        <th>Lead Name</th>
+        <th>Company Name</th>
+        <th>Category</th>
+        <th>Area</th>
+        <th>Payment</th>
+        <th>Date</th>
+        </thead>
+        <tbody>
+            @foreach($bills as $bill)
+                <tr>
+                    <th>{{$bill->leadName}}</th>
+                    <th>{{$bill->companyName}}</th>
+                    <th>{{$bill->categoryName}}</th>
+                    <th>{{$bill->areaName}}</th>
+                    <th>{{$bill->paymentInserted}}</th>
+                    <th>{{$bill->paymentDate}}</th>
 
-            </tr>
-        @endforeach
-    </tbody>
-</table>
+                </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
