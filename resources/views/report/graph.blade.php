0f9d20771dc9e448d32f90e420a455dacef3d1a9
Sazzad171 <sajjad.cse.diu@gmail.com>
2021-11-09 14:49:57 +0600

    update

--- resources/views/report/graph.blade.php
+++ resources/views/report/graph.blade.php
@@ -10,13 +10,20 @@
     <div class="card">
         <div class="card-body">
             <label><b>Search</b></label>
-            <label><b>Search</b></label>
             <form method="post" action="{{route('searchGraphByDate')}}">
                 {{csrf_field()}}
-                <input type="text" placeholder=" From" id="fromdate" name="fromDate" style="border-radius: 50px;"
-                       required>
-                <input type="text" placeholder=" To" id="todate" name="toDate" style="border-radius: 50px;" required>
-                <button type="submit" class="btn btn-success">Search</button>
+                <div class="row">
+                    <div class="col-auto mb-2">
+                        <input type="text" placeholder=" From" id="fromdate" name="fromDate" style="border-radius: 50px;"
+                        required>
+                    </div>
+                    <div class="col-auto mb-2">
+                        <input type="text" placeholder=" To" id="todate" name="toDate" style="border-radius: 50px;" required>
+                    </div>
+                    <div class="col-md-5 mb-2">
+                        <button type="submit" class="btn btn-success">Search</button>
+                    </div>
+                </div>
             </form>
 
             <div id="chartContainer" style="height: 600px; width:100%;"></div>
