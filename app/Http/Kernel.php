9a40d76e1da58f73b74b65ae6b28aff8ad679ff5
Asif-Abrar <asifabrar2@gmail.com>
2024-01-02 15:25:13 +0600

    revenue route limited

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -58,6 +58,7 @@ class Kernel extends HttpKernel
         'bindings' => \Illuminate\Routing\Middleware\SubstituteBindings::class,
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
+        'check.admin' => \App\Http\Middleware\CheckIfAdmin::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
     ];
 }
